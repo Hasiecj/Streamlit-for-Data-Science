0387e9f9f14f63b4be573d31e82b973b4ed368a6
hasiecj <jakub.hasiec@ema.europa.eu>
2024-07-26 17:49:25 +0200

    Updates chapters 1-3

--- penguin_app/penguins.py
+++ penguin_app/penguins.py
@@ -2,24 +2,52 @@ import altair as alt
 import pandas as pd
 import seaborn as sns
 import streamlit as st
+import os
+import time
+
+# os.chdir("./penguin_app")
 
 st.title("Palmer's Penguins")
-st.markdown("Use this Streamlit app to make your own scatterplot about penguins!")
+st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 
-selected_x_var = st.selectbox(
-    "What do you want the x variable to be?",
-    ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
-)
-selected_y_var = st.selectbox(
-    "What about the y?",
-    ["bill_depth_mm", "bill_length_mm", "flipper_length_mm", "body_mass_g"],
-)
+#import our data
+# penguins_df = pd.read_csv('penguins.csv')
 
-penguin_file = st.file_uploader("Select Your Local Penguins CSV")
-if penguin_file is not None:
-    penguins_df = pd.read_csv(penguin_file)
+#Columns of our interest
+#penguins_df.columns[2:6]
+
+penguin_file = st.file_uploader(
+    'Select Your Local Penguins CSV (default provided)')
+
+@st.cache_data()
+def load_file(penguin_file):
+    if penguin_file is not None:
+        df = pd.read_csv(penguin_file)
+    else:
+        df = pd.read_csv('penguins.csv')
+    return(df)
+penguins_df = load_file(penguin_file)
+
+sns.set_style('darkgrid')
+markers = {"Adelie": "X", "Gentoo": "s", "Chinstrap":'o'}
+
+# selected_species = st.selectbox('What species would you like to visualize?',
+#      penguins_df['species'].unique())
+selected_x_var = st.selectbox('What do you want the x variable to be?',
+     penguins_df.columns[2:6])
+selected_y_var = st.selectbox('What about the y?',
+     penguins_df.columns[2:6])
+
+selected_gender = st.selectbox('What gender do you want to filter for?',
+                               ['all penguins', 'male penguins', 'female penguins'])
+if selected_gender == 'male penguins':
+    penguins_df = penguins_df[penguins_df['sex'] == 'male']
+elif selected_gender == 'female penguins':
+    penguins_df = penguins_df[penguins_df['sex'] == 'female']
 else:
-    st.stop()
+    pass
+
+# penguins_df = penguins_df[penguins_df['species'] == selected_species] 
 
 alt_chart = (
     alt.Chart(penguins_df, title="Scatterplot of Palmer's Penguins")
--- trees_app/trees.py
+++ trees_app/trees.py
@@ -1,14 +1,104 @@
+#Built-in visuazizations methods
+
+import numpy as np
 import pandas as pd
 import streamlit as st
-
+ 
 st.title("SF Trees")
 st.write(
-    "This app analyses trees in San Francisco using"
-    " a dataset kindly provided by SF DPW"
+    """This app analyzes trees in San Francisco using
+    a dataset kindly provided by SF DPW"""
 )
 trees_df = pd.read_csv("trees.csv")
-df_dbh_grouped = pd.DataFrame(trees_df.groupby(["dbh"]).count()["tree_id"])
-df_dbh_grouped.columns = ["tree_count"]
-st.line_chart(df_dbh_grouped)
+df_dbh_grouped = pd.DataFrame(
+    trees_df.groupby(["dbh"]).count()["tree_id"]
+).reset_index()
+df_dbh_grouped.columns = ["dbh", "tree_count"]
+st.line_chart(df_dbh_grouped, x="dbh", y="tree_count")
 st.bar_chart(df_dbh_grouped)
 st.area_chart(df_dbh_grouped)
+
+#Plotly visualizations
+# import streamlit as st
+# import pandas as pd
+# import plotly.express as px
+# st.title('SF Trees')
+# st.write(
+#     """This app analyzes trees in San Francisco using
+#     a dataset kindly provided by SF DPW"""
+# )
+# st.subheader('Plotly Chart')
+# trees_df = pd.read_csv('trees.csv')
+# fig = px.histogram(trees_df['dbh'])
+# st.plotly_chart(fig)
+
+
+#Matplotlib and Seaborn
+# import streamlit as st
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import datetime as dt
+# st.title('SF Trees')
+# st.write(
+#     """This app analyzes trees in San Francisco using
+#     a dataset kindly provided by SF DPW"""
+# )
+# trees_df = pd.read_csv('trees.csv')
+# trees_df['age'] = (pd.to_datetime('today') -
+#                    pd.to_datetime(trees_df['date'])).dt.days
+# st.subheader('Seaborn Chart')
+# fig_sb, ax_sb = plt.subplots()
+# ax_sb = sns.histplot(trees_df['age'])
+# plt.xlabel('Age (Days)')
+# st.pyplot(fig_sb)
+# st.subheader('Matploblib Chart')
+# fig_mpl, ax_mpl = plt.subplots()
+# ax_mpl = plt.hist(trees_df['age'])
+# plt.xlabel('Age (Days)')
+# st.pyplot(fig_mpl)
+
+#Bokeh
+# import streamlit as st
+# import pandas as pd
+# from bokeh.plotting import figure
+# st.title('SF Trees')
+# st.write('This app analyzes trees in San Francisco using'
+#         ' a dataset kindly provided by SF DPW')
+# st.subheader('Bokeh Chart')
+# trees_df = pd.read_csv('trees.csv')
+# scatterplot = figure(title = 'Bokeh Scatterplot')
+# scatterplot.scatter(trees_df['dbh'], trees_df['site_order'])
+# scatterplot.yaxis.axis_label = "site_order"
+# scatterplot.xaxis.axis_label = "dbh"
+# st.bokeh_chart(scatterplot)
+
+#Altair
+
+#Option 1
+# import streamlit as st
+# import pandas as pd
+# import altair as alt
+# st.title('SF Trees')
+# st.write(
+#     """This app analyzes trees in San Francisco using
+#     a dataset kindly provided by SF DPW"""
+# )
+# trees_df = pd.read_csv('trees.csv')
+# df_caretaker = trees_df.groupby(['caretaker']).count()['tree_id'].reset_index()
+# df_caretaker.columns = ['caretaker', 'tree_count']
+# fig = alt.Chart(df_caretaker).mark_bar().encode(x = 'caretaker', y = 'tree_count')
+# st.altair_chart(fig)
+
+#Option 2
+# import streamlit as st
+# import pandas as pd
+# import altair as alt
+# st.title('SF Trees') 
+# st.write(
+#     """This app analyzes trees in San Francisco using
+#     a dataset kindly provided by SF DPW"""
+# )
+# trees_df = pd.read_csv('trees.csv')
+# fig = alt.Chart(trees_df).mark_bar().encode(x = 'caretaker', y = 'count(*):Q')
+# st.altair_chart(fig)
